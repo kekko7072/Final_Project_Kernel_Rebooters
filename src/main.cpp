7ee03fdefefc062623cd239efd3d67b091c41163
Luca Pellegrini <116035480+luca-pellegrini@users.noreply.github.com>
2025-08-26 22:20:26 +0200

    Update main.cpp
    Split task between group members

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,20 @@ using std::endl;
 
 int main(int argc, char *argv[])
 {
-    // Preprocessing
-    flower_category();        // Let the user choose which type of flower he wants to analyze
+    // Preprocessing --> Luca
     import_images();          // Import the test images of the chosen flower type
     extract_descriptors();    // Extract global and local descriptor from the test image
 
     // Processing
-    sift();                   // Compare the feature of the test image with the ones from the train folder using SIFT
-    surf();                   // Compare the feature of the test image with the ones from the train folder using SURF
-    tm();                     // Compare the feature of the test image with the ones from the train folder using template matching
-    hog();                    // Compare the feature of the test image with the ones from the train folder using Histogram Orientation Gradient
-    bow();                    // Compare the feature of the test image with the ones from the train folder using Bag Of Words
-    is_health();              // Check if the test flower is healthy or not (maybe looking at the colors of the flower?)
+    for each test_image :
+        sift(); // Marco          // Compare the feature of the test image with the ones from the train folder using SIFT
+        surf(); // Marco          // Compare the feature of the test image with the ones from the train folder using SURF
+        tm();   // Luca           // Compare the feature of the test image with the ones from the train folder using template matching
+        hog();  // Francesco      // Compare the feature of the test image with the ones from the train folder using Histogram Orientation Gradient
+        bow();  // Francesco      // Compare the feature of the test image with the ones from the train folder using Bag Of Words
+        is_health();              // Check if the test flower is healthy or not (maybe looking at the colors of the flower?)
 
-    // Performance measurement
+    // Performance measurement --> Marco
     print_output();           // Display the image with a bounding box and a similarity score near it
     total_acc();              // Calculate the entire test set to quantify the overall robustness of the system: Number of images correctly predicted / number of test images     
     category_acc();           // Percentage of correct classification for each of the 5 categories: Number of images correctly predicted for a category / number of test images of that category
