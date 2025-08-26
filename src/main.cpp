08c8b2df2980a26f254b3c5a437b584ce74a9bd3
Marco Carraro <marco.carraro.23@studenti.unipd.it>
2025-08-26 15:34:18 +0200

    Initial files specification

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <filesystem>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+namespace fs = std::filesystem;
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    // Preprocessing
+    flower_category();        // Let the user choose which type of flower he wants to analyze
+    import_images();          // Import the test images of the chosen flower type
+    extract_descriptors();    // Extract global and local descriptor from the test image
+
+    // Processing
+    sift();                   // Compare the feature of the test image with the ones from the train folder using SIFT
+    surf();                   // Compare the feature of the test image with the ones from the train folder using SURF
+    tm();                     // Compare the feature of the test image with the ones from the train folder using template matching
+    hog();                    // Compare the feature of the test image with the ones from the train folder using Histogram Orientation Gradient
+    bow();                    // Compare the feature of the test image with the ones from the train folder using Bag Of Words
+    is_health();              // Check if the test flower is healthy or not (maybe looking at the colors of the flower?)
+
+    // Performance measurement
+    print_output();           // Display the image with a bounding box and a similarity score near it
+    total_acc();              // Calculate the entire test set to quantify the overall robustness of the system: Number of images correctly predicted / number of test images     
+    category_acc();           // Percentage of correct classification for each of the 5 categories: Number of images correctly predicted for a category / number of test images of that category
+    mean_processing_time();   // Average time required to classify an input image (maybe with pipobarra while processing the processing?)
+
+    return 0;
+}
