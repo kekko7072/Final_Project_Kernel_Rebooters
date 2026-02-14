46978e0e8f25046fbbf48aaa53296f0489b63043
Marco Carraro <marco.carraro.23@studenti.unipd.it>
2026-02-14 20:05:27 +0100

    update flower_type.hpp to avoid segmentation fault

--- include/flower_type.hpp
+++ include/flower_type.hpp
@@ -2,6 +2,11 @@
 #ifndef FLOWER_TYPE_HPP
 #define FLOWER_TYPE_HPP
 
-enum class FlowerType {Daisy = 1, Dandelion, Rose, Sunflower, Tulip, NoFlower = -1};
+enum class FlowerType {Daisy = 0,
+                       Dandelion = 1, 
+                       Rose = 2, 
+                       Sunflower = 3, 
+                       Tulip = 4, 
+                       NoFlower = 5};
 
 #endif // FLOWER_TYPE_HPP
